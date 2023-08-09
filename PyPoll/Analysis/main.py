5eb8974913a0f220914dfe6934c483f6f8e07117
cmorrow28 <cayleymorrow1@gmail.com>
2023-08-09 21:25:29 +0800

    git push origin main

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -3,21 +3,58 @@ import csv
 
 election_data = os.path.join('..', 'Resources', 'election_data.csv')
 
-voters_list = []
-voter_id = 0
+#defining variables
+total_votes = 0
+C_C_S = "Charles Casper Stockham"
+Stockham_votes = 0
+D_D = "Diana DeGette"
+DeGette_votes = 0
+R_A_D = "Raymon Anthony Doane"
+Doane_votes = 0
 
-with open(election_data, 'r') as csvfile:
+with open(election_data, 'r') as csvfile: #opening csv file to read the data
     csv_reader = csv.reader(csvfile)
 
-    header = next(csv_reader)
+    header = next(csv_reader) #skipping data in header row and storing as a variable
 
     for row in csv_reader:
-        voter_id += int(row[0])
-        voters = row[0] 
-        if voters not in voters_list:
-            voters_list.append(voters)
+        #calculate total voters
+        total_votes += 1
+        #calculate votes for the candidates
+        if row[2] == C_C_S:
+            Stockham_votes += 1
+        elif row[2] == D_D:
+            DeGette_votes += 1
+        elif row[2] == R_A_D:
+            Doane_votes += 1
+    
+    #calculate percentages after loop has stopped counting
+    Stockham_percent = (Stockham_votes / total_votes) * 100
+    DeGette_percent = (DeGette_votes / total_votes) * 100
+    Doane_percent = (Doane_votes / total_votes) * 100
+    
+    #make a dictionary of the variables
+    candidate_list = {
+        (Stockham_percent): C_C_S,
+        (DeGette_percent): D_D,
+        (Doane_percent): R_A_D
+    }
+    #create a variable that locates the winner
+    winner = (candidate_list.get(max(candidate_list)))
 
-total_voters = len(voters_list)
-print("Total Votes: " + str(total_voters))
-
-
+print(f"""
+Election Results
+------------------------------
+Total Votes: {total_votes}
+-------------------------------
+Charles Casper Stockham: {Stockham_percent:.3f}% ({Stockham_votes}) 
+Diana DeGette: {DeGette_percent:.3f}% ({DeGette_votes})
+Raymon Anthony Doane: {Doane_percent:.3f}% ({Doane_votes})
+-------------------------------
+Winner: {winner}
+-------------------------------
+""")
+#----------------------------------------------------------------------------------
+#file = os.path.join('..', 'Analysis', 'text_output.txt')
+#with open(file, 'w') as textfile:
+    #textfile.write(output)
